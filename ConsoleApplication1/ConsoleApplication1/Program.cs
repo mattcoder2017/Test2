10b7e82cee88f2fc28367413c3a94acddc42b884
mattcoder2017 <myang@arris.com>
2018-10-04 15:43:11 +0800

    Add Master 2
    Add Master 2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> VersionTags = new Dictionary<string, string>();
+            VersionTags.Add("Master", "1");
+            VersionTags.Add("Master", "2");
+            foreach (var obj in VersionTags)
+            {
+                Console.WriteLine("Key : {0} Value : {1}", new String[]{obj.Key, obj.Value });
+            }
+            Console.Read();
+        }
+
+    }
+}
